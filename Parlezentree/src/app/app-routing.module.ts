e675402614badd603238b3a4b3b3d63f7836c44c
Markfon <m4rkfons@outlook.com>
2022-05-26 15:41:23 -0400

    Routing

---
+++ Parlezentree/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { RouterModule } from "@angular/router";
+import { RegisterComponent } from "./register/register.component";
+import { LoginComponent } from "./login/login.component";
+import { IngredientsComponent } from "./ingredients/ingredients.component";
+import { SearchRecipeComponent } from "./search-recipe/search-recipe.component";
+import { JokeComponent } from "./joke/joke.component";
+import { ProfileComponent } from "./profile/profile.component";
+
+const routes = [
+    {path: '', redirectTo : '/login', pathMatch:'full'},
+    {path: 'home', component: HomePageComponent},
+    {path : 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'ingredients', component: IngredientsComponent},
+    {path: 'search-recipe', component: SearchRecipeComponent},
+    {path: 'joke', component: JokeComponent},
+    {path: 'profile', component: ProfileComponent}
+
+]
+
+//Start Building NgModule to create routing
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{}

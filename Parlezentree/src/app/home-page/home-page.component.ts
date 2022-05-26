c510d419681f7b8d5e22b80078ddd8265619596d
Harsh4598 <harsh.ht008@gmail.com>
2022-05-26 16:18:53 -0400

    add navbar service to hide or show navbar

--- Parlezentree/src/app/home-page/home-page.component.ts
+++ Parlezentree/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarService } from '../navbar.service';
 
 @Component({
   selector: 'home-page',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private nav:NavbarService) { }
 
   ngOnInit(): void {
+   this.nav.show();
   }
 
 }

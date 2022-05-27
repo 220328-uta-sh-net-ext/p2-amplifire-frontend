e6fd8ec15d4735d4b1a6591576709b10dfaa9319
Harsh4598 <harsh.ht008@gmail.com>
2022-05-26 22:00:12 -0400

    add ui for recipe search by indredients

--- Parlezentree/src/app/ingredients/ingredients.component.ts
+++ Parlezentree/src/app/ingredients/ingredients.component.ts
@@ -6,8 +6,16 @@ import { NavbarService } from '../navbar.service';
   templateUrl: './ingredients.component.html',
   styleUrls: ['./ingredients.component.css']
 })
+
 export class IngredientsComponent implements OnInit {
 
+  ingrediants:string =  "";
+
+   search():void{
+       alert(this.ingrediants);
+       this.ingrediants="";
+    }
+    
   constructor(private nav:NavbarService) { }
 
   ngOnInit(): void {
